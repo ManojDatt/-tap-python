d2551bb4075b149c118b75ae462f6adaec5ece44
Manoj datt <Manojdatt1it@gmail.com>
2024-02-27 19:05:10 +0530

    Update card.py

--- examples/card.py
+++ examples/card.py
@@ -29,32 +29,6 @@ print('Success: %r' % (resp))
 token_id = resp.id
 
 ######################
-# TODO REMOVE THIS
-######################
-
-data = {
-  "card": {
-    "number": 4508750015741019,
-    "exp_month": 12,
-    "exp_year": 21,
-    "cvc": 124,
-    "name": "test token",
-    "address": {
-      "country": "Kuwait",
-      "line1": "Salmiya, 21",
-      "city": "Kuwait city",
-      "street": "Salim",
-      "avenue": "Gulf"
-    }
-  },
-  "client_ip": "192.168.1.20"
-}
-
-resp = tap.Token.create(**data)
-print('Success: %r' % (resp))
-new_card_token_id = resp.id
-
-######################
 # Customer Creation
 ######################
 
@@ -70,7 +44,7 @@ resp = tap.Customer.create(**data)
 customer_id = resp.id
 
 ######################
-# Create Card
+# Optional Create Card
 ######################
 resp = tap.Customer.create_card(resp.id, **{'source': token_id})
 card_id = resp.id
